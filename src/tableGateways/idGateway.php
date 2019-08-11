80abd7d010d420cf6a13248c5ef8cb1986364798
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-08-11 09:15:32 +0300

    Update idGateway.php

--- src/tableGateways/idGateway.php
+++ src/tableGateways/idGateway.php
@@ -7,15 +7,21 @@ class IdGateway {
     }
 
     public function add_new_code($values) {
-        $this->execute("insert into `code_ids` (`CodeID`, `Code`) values ('$values[0]', '$values[1]')");
+        $this->execute("insert into `code_ids` (`CodeID`, `Code`) values ('$values[CodeID]', '$values[Code]')");
     }
 
     public function update_code($values) {
-        $this->execute("update `code_ids` set `Code` = '$values[1]' where `CodeID` = '$values[0]'");
+        $this->execute("update `code_ids` set `Code` = '$values[Code]' where `CodeID` = '$values[CodeID]'");
     }
 
     public function get_code_by_id($id) {
-        return $this->execute("select * from `code_ids` where CodeID = '$id'");
+        $result = $this->execute("select * from `code_ids` where CodeID = '$id'");
+        $array = array();
+
+        while($row = mysqli_fetch_assoc($result))
+            $array[] = $row;
+
+        return json_encode($array);
     }
 
     public function delete($id) {
