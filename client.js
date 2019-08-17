2d3bd2f83d7f78db1cd6d532f21be157c779090e
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-08-17 08:31:37 +0300

    Create client.js

---
+++ client.js
@@ -0,0 +1,33 @@
+function insertNewCode(id, code) {
+    const http = new XMLHttpRequest();
+
+    http.open("POST", 'http://100.66.121.164:5520/code', true);
+    http.send(JSON.stringify({"CodeID": id, "Code": code}));
+}
+
+function updateCode(id, code) {
+    const http = new XMLHttpRequest();
+
+    http.open("PUT", `http://100.66.121.164:5520/code/${id}`, true);
+    http.send(JSON.stringify({"CodeID": id, "Code": code}));
+}
+
+function deleteCode(id) {
+    const http = new XMLHttpRequest();
+
+    http.open("DELETE", `http://100.66.121.164:5520/code/${id}`, true);
+    http.send();
+}
+
+function getCode(id) {
+    const http = new XMLHttpRequest();
+
+    http.onreadystatechange = () => {
+        if (http.readyState == 4 && http.status == 200) console.log(JSON.parse(JSON.parse(http.responseText).slice(1, -1)).Code);
+    }
+
+    http.open("GET", `http://100.66.121.164:5520/code/${id}`, true);
+    http.send();
+}
+
+getCode(2);
