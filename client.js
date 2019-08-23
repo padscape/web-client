aae33881ea1c84dd6a1958a7d13828dc8db8d706
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-08-23 11:19:01 +0300

    Update client.js

--- client.js
+++ client.js
@@ -1,21 +1,21 @@
 function insertNewCode(id, code) {
     const http = new XMLHttpRequest();
 
-    http.open("POST", 'http://100.66.121.164:5520/code', true);
+    http.open("POST", 'http://100.73.27.89:5520/code', true);
     http.send(JSON.stringify({"CodeID": id, "Code": code}));
 }
 
 function updateCode(id, code) {
     const http = new XMLHttpRequest();
 
-    http.open("PUT", `http://100.66.121.164:5520/code/${id}`, true);
+    http.open("PUT", `http://100.73.27.89:5520/code/${id}`, true);
     http.send(JSON.stringify({"CodeID": id, "Code": code}));
 }
 
 function deleteCode(id) {
     const http = new XMLHttpRequest();
 
-    http.open("DELETE", `http://100.66.121.164:5520/code/${id}`, true);
+    http.open("DELETE", `http://100.73.27.89:5520/code/${id}`, true);
     http.send();
 }
 
@@ -26,8 +26,6 @@ function getCode(id) {
         if (http.readyState == 4 && http.status == 200) console.log(JSON.parse(JSON.parse(http.responseText).slice(1, -1)).Code);
     }
 
-    http.open("GET", `http://100.66.121.164:5520/code/${id}`, true);
+    http.open("GET", `http://100.73.27.89:5520/code/${id}`, true);
     http.send();
 }
-
-getCode(2);
