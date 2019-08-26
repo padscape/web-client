ea6799db994f8fc76304877fbd2398319dce4948
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-08-26 16:32:01 +0300

    Update client.js

--- client.js
+++ client.js
@@ -2,14 +2,14 @@ function insertNewCode(id, code) {
     const http = new XMLHttpRequest();
 
     http.open("POST", 'http://100.73.27.89:5520/code', true);
-    http.send(JSON.stringify({"CodeID": id, "Code": code}));
+    http.send(JSON.stringify({"CodeID": id, "Code": code, "Creator": creator}));
 }
 
 function updateCode(id, code) {
     const http = new XMLHttpRequest();
 
     http.open("PUT", `http://100.73.27.89:5520/code/${id}`, true);
-    http.send(JSON.stringify({"CodeID": id, "Code": code}));
+    http.send(JSON.stringify({"CodeID": id, "Code": code, "Creator": creator}));
 }
 
 function deleteCode(id) {
