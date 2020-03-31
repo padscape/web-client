08ba7f071a37df6e221e1788debbe2450d4b6d9d
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2020-03-31 18:41:55 +0300

    Update connection.js

--- src/connection.js
+++ src/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "k@roto",
+    password: "SECRET",
     database: "padscape"
 });
 
