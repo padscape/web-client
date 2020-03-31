ceefd1cbd2d4c6980b9adb7c55029b2cb6d00fe1
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2020-03-31 09:02:51 +0300

    Update client.js

--- client.js
+++ client.js
@@ -1,42 +1,41 @@
-function insertNewCode(id, code) {
+insertNewCode = (code, creator) => {
     const http = new XMLHttpRequest();
-
-    http.open("POST", 'http://100.73.27.89:5520/code', true);
-    http.send(JSON.stringify({"CodeID": id, "Code": code}));
+    http.open("POST", 'http://127.0.0.1:8080/code', true);
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    http.send(`Code=${code}&Creator=${creator}`);
 }
 
-function updateCode(id, code) {
+updateCode = (id, code, creator) => {
     const http = new XMLHttpRequest();
-
-    http.open("PUT", `http://100.73.27.89:5520/code/${id}`, true);
-    http.send(JSON.stringify({"CodeID": id, "Code": code}));
+    http.open("PUT", `http://127.0.0.1:8080/code/${id}`, true);
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    http.send(`Code=${code}&Creator=${creator}`);
 }
 
-function deleteCode(id) {
+deleteCode = id => {
     const http = new XMLHttpRequest();
-
-    http.open("DELETE", `http://100.73.27.89:5520/code/${id}`, true);
+    http.open("DELETE", `http://127.0.0.1:8080/code/${id}`, true);
     http.send();
 }
 
-function getCode(id) {
+getCode = id => {
     const http = new XMLHttpRequest();
 
     http.onreadystatechange = () => {
-        if (http.readyState == 4 && http.status == 200) console.log(JSON.parse(JSON.parse(http.responseText).slice(1, -1)).Code);
+        if (http.readyState == 4 && http.status == 200) console.log(http.responseText);
     }
 
-    http.open("GET", `http://100.73.27.89:5520/code/${id}`, true);
+    http.open("GET", `http://127.0.0.1:8080/code/${id}`, true);
     http.send();
 }
 
-function getCode() {
+getCode = () => {
     const http = new XMLHttpRequest();
 
     http.onreadystatechange = () => {
-        if (http.readyState == 4 && http.status == 200) console.log(Number(JSON.parse(JSON.parse(http.responseText).slice(1, -1).split(',').pop()).CodeID) + 1);
+        if (http.readyState == 4 && http.status == 200) console.log(http.responseText);
     }
 
-    http.open("GET", `http://100.73.27.89:5520/code`, true);
+    http.open("GET", `http://127.0.0.1:8080/code`, true);
     http.send();
 }
