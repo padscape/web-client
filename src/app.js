4b755b0dddc4bec33ec75a5d7bc9a9ccc87565bc
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2020-04-02 14:02:12 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const multer = require('multer');
+const https = require('https');
 const cors = require('cors');
+const fs = require('fs');
 const upload = multer();
 const app = express();
 const codes = require('./codes.js');
+
+let privateKey = fs.readFileSync('private.key', 'utf-8');
+let certificate = fs.readFileSync('server.cert', 'utf-8');
+let credentials = {key: privateKey, cert: certificate};
 
 app.get('/', (req, res) => {
     res.writeHead(400, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
@@ -12,10 +18,11 @@ app.get('/', (req, res) => {
 });
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(express.static(__dirname, {dotfiles: 'allow'}));
 app.use(bodyParser.json());
 app.use(upload.array());
 app.use(cors());
 app.use('/code', codes);
 
-app.listen(8080);
+let httpsServer = https.createServer(credentials, app);
+httpsServer.listen(443);
