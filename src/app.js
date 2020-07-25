9ecc61d7d3e27b4e7b0bb961646bb782f197858f
BonfireScratch <sigsaleum@gmail.com>
2020-07-25 09:28:56 +0300

    Add account verification page

--- src/app.js
+++ src/app.js
@@ -17,25 +17,25 @@ app.use(session({
 }));
 
 app.get('/', (req, res) => {
-    console.log('Is activated: ' + req.session.pendingActivation);
+    // console.log('Is activated: ' + req.session.pendingActivation);
 
-    if (req.session.pendingActivation) {
-        res.send(`Activate your account, ${req.session.username}!`);
-        res.end();
-        return;
-    } else {
-        req.session.pendingActivation = false;
-    }
+    // if (req.session.pendingActivation) {
+    //     res.sendFile(path.join(__dirname + '/website/activate.html'));
+    //     return;
+    // } else {
+    //     req.session.pendingActivation = false;
+    // }
     
-    if (req.session.loggedin) {
-        res.send(`Welcome back, ${req.session.username}!`);
-        res.end();
-        return;
-    }
+    // if (req.session.loggedin) {
+    //     res.send(`Welcome back, ${req.session.username}!`);
+    //     res.end();
+    //     return;
+    // }
     
-    req.session.loggedin = false;
-    req.session.username = '';
-    res.sendFile(path.join(__dirname + '/website/home.html'));
+    // req.session.loggedin = false;
+    // req.session.username = '';
+    // res.sendFile(path.join(__dirname + '/website/home.html'));
+    res.sendFile(path.join(__dirname + '/website/activate.html'));
 });
 
 app.get('/login', (req, res) => {
